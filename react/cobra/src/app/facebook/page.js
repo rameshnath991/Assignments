a6c2038dd8bc29a26e0466ad24d56960c22faa87
rameshnath991 <rameshnath991@gmail.com>
2024-05-06 08:08:17 +0545

    like button add

--- react/cobra/src/app/facebook/page.js
+++ react/cobra/src/app/facebook/page.js
@@ -4,20 +4,20 @@ import { GrLike } from "react-icons/gr";
 
 const page = () => {
     let [color, setColor]= useState('grey')
-    let [LikeUnlike, setLikeUnlike] = useState('Unlike ')
+   
     let handleColorChange = ()=>{
       if (color == "grey"){
         setColor("blue")
-        setLikeUnlike('Like')
+       
     }else {
         setColor("grey")
-        setLikeUnlike('Unlike')
+     
     }
     }
   return (
     <div>
         <button onClick={()=>handleColorChange()} className='bg-slate-50 p-2 border '>
-        {LikeUnlike}<GrLike color={color}/> 
+        <GrLike color={color}/> 
             </button>
     </div>
   )
